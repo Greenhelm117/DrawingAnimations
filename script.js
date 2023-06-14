107e57055881d88f0202bb124ef1e7047854b98b
Greenhelm <andrew.b.sheils@gmail.com>
2023-06-14 18:21:30 -0400

    timer class and methods added to seperate file

--- script.js
+++ script.js
@@ -10,37 +10,18 @@ console.log("HELLO THERE");
 
 //animation uses same loop I guess and ticks down as well
 
-class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    this.startButton.addEventListener("click", this.start);
-    this.pauseButton.addEventListener("click", this.pause);
-  }
-
-  start = () => {
-    this.tick;
-    this.interval = setInterval(this.tick, 1000);
-  };
-
-  tick = () => {
-    const timeRemaining = parseFloat(this.durationInput.value);
-    this.durationInput.value = timeRemaining - 1;
-
-    if (this.durationInput.value == 0) {
-      clearInterval(this.interval);
-    }
-  };
-
-  pause = () => {
-    clearInterval(this.interval);
-  };
-}
-
 const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart() {
+    console.log("Timer started");
+  },
+  onTick() {
+    console.log("Timer just ticked down");
+  },
+  onComplete() {
+    console.log("Timer completed");
+  },
+});
